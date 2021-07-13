d038c9b7c3afd7b56f38b51aeb43db7a9fa029c1
Javier Tibi <javier@digitalplankton.co>
2021-07-13 18:05:02 -0400

    server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
 app.use(serveStatic(__dirname + "/dist"))
+console.log('server started ' + __dirname)
 var port = process.env.PORT || 80
 app.listen(port)
 console.log('server started ' + port)
