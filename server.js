6f77dae0d91e15f7943b6410f0355a76696eea56
Javier Tibi <javier@digitalplankton.co>
2021-07-13 17:39:48 -0400

    server js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front-angular'));
+app.use(express.static('./dist/crowdfunding-yanapana'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front-angular/'}),
+    res.sendFile('index.html', {root: 'dist/crowdfunding-yanapana/'}),
 );
 
 // Start the app by listening on the default Heroku port
