9322efb0045f0d1ae6de5cc1832feca72b744f3f
Javier Tibi <javier@digitalplankton.co>
2021-08-11 13:42:59 -0400

    Ritmo Xeneize

--- src/app/campaing.ts
+++ src/app/campaing.ts
@@ -4,4 +4,9 @@ export interface Campaign {
     category: string;
     brief: string;
     story: string;
+    img: string;
+    btn_mp: string;
+    user: string;
+    monto: string;
+    ig: string;
 }
