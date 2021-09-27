855937360fc1c8261b7e6daff5eabca6f3df1671
Javier Tibi <javier@digitalplankton.co>
2021-09-27 17:05:11 -0300

    Add btns mp

--- src/app/campaing.ts
+++ src/app/campaing.ts
@@ -6,6 +6,9 @@ export interface Campaign {
     story: string;
     img: string;
     btn_mp: string;
+    btn_mp_50: string;
+    btn_mp_200: string;
+    btn_mp_500: string;
     user: string;
     monto: string;
     ig: string;
